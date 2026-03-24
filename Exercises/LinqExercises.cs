f3170e5c3e0894d46166172362c9a2bd413f2f35
Anton <koshkai2005@gmail.com>
2026-03-24 11:11:12 +0100

    Task08

--- Exercises/LinqExercises.cs
+++ Exercises/LinqExercises.cs
@@ -138,9 +138,11 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task08_DistinctStudentCities()
     {
-        throw NotImplemented(nameof(Task08_DistinctStudentCities));
+        return UniversityData.Students
+            .Select(s => s.City)
+            .Distinct()
+            .OrderBy(city => city);
     }
-
     /// <summary>
     /// Task:
     /// Return the three newest enrollments.
