c5bbf52420990aaca4d28f1b857b23f12f24ebb2
Anton <koshkai2005@gmail.com>
2026-03-24 11:14:54 +0100

    Task10

--- Exercises/LinqExercises.cs
+++ Exercises/LinqExercises.cs
@@ -173,9 +173,12 @@ public sealed class LinqExercises
     /// </summary>
     public IEnumerable<string> Task10_SecondPageOfCourses()
     {
-        throw NotImplemented(nameof(Task10_SecondPageOfCourses));
+        return UniversityData.Courses
+            .OrderBy(c => c.Title)
+            .Skip(2)
+            .Take(2)
+            .Select(c => $"{c.Title} | {c.Category}");
     }
-
     /// <summary>
     /// Task:
     /// Join students with enrollments by StudentId.
